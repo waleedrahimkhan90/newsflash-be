b5cf94c58fccf14d7caac93ff92808d965312ee4
waleedrahimkhan90 <waleedrkhan90@gmail.com>
2025-06-15 02:41:24 +0500

    added another news

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ app = FastAPI()
 
 news_data = [
     {"id": 1, "title": "First News", "content": "This is the first news item."},
-    {"id": 2, "title": "Second News", "content": "This is the second news item."}
+    {"id": 2, "title": "Second News", "content": "This is the second news item."},
+    {"id": 3, "title": "Second News", "content": "This is the third news item."},
 ]
 
 @app.get("/news")
